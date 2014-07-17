7853617a69abb23a45dec191ea6268fde8fbd17d
William Antonio <william.a.siqueira@gmail.com>
2014-07-17 00:56:16 -0300

    REST method to save Person

--- web-module/src/main/java/org/jugvale/peoplemanagement/rest/PersonResource.java
+++ web-module/src/main/java/org/jugvale/peoplemanagement/rest/PersonResource.java
@@ -4,10 +4,13 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.jugvale.peoplemanagement.model.Person;
 import org.jugvale.peoplemanagement.service.PersonService;
@@ -25,5 +28,11 @@ public class PersonResource {
 	@Path("rfid/{rfid}")
 	public Person getPersonByRfid(@PathParam("rfid") String rfid){
 		return service.findByRFID(rfid);
+	}
+	
+	@POST
+	public Response addPerson(Person p){
+		service.save(p);
+		return Response.status(Status.CREATED).build();
 	}
 }
