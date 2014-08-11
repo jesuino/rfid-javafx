751eaea73dfcafeed044fb7685fd623cd4879fe6
William Antonio <william.a.siqueira@gmail.com>
2014-08-10 23:00:01 -0300

    Adding style and finishing the form

--- client-module/src/org/jugvale/peoplemanagement/client/controller/RegisterController.java
+++ client-module/src/org/jugvale/peoplemanagement/client/controller/RegisterController.java
@@ -7,6 +7,9 @@ package org.jugvale.peoplemanagement.client.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -22,6 +25,8 @@ import org.jugvale.peoplemanagement.client.service.PersonService;
  * @author william
  */
 public class RegisterController implements Initializable {
+
+    final String SCAN_RFID_MESSAGE = "Click here to scan a new RFID";
 
     @FXML
     Pane formPane;
@@ -46,8 +51,9 @@ public class RegisterController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        lblRFID.setText(SCAN_RFID_MESSAGE);
         rfidPane = new FTDIReaderPane(s -> {
-            lblRFID.setText("RFID: " + s + " (click to scan)");
+            lblRFID.setText("RFID: " + s + " (click to re-scan)");
             lblRFID.setUserData(s);
         }, lblStatus::setText);
         rfidPane.setVisible(false);
@@ -57,19 +63,47 @@ public class RegisterController implements Initializable {
     }
 
     public void save() {
-        Person toSave = new Person(
-                txtFirstName.getText(),
-                txtLastName.getText(),
-                String.valueOf(lblRFID.getUserData()),
-                txtJob.getText(),
-                Integer.parseInt(txtAge.getText())
-        );
-        service.save(toSave, p -> {
-            lblStatus.setText("Person " + p.getFirstName() + " with RFID " + p.getRfid() + " saved with success");
+        getPersonIfValid(valided -> {
+            service.save(valided, p -> {
+                clearForm();
+                lblStatus.setText(String.format("Person %s with RFID %s saved with success", p.getFirstName(), p.getRfid()));
+            }, lblStatus::setText);
         }, lblStatus::setText);
     }
 
     public void showRfidPane() {
         rfidPane.askForDeviceAndReadTag();
+    }
+
+    private void clearForm() {
+        Stream.of(txtAge, txtFirstName, txtJob, txtLastName).forEach(t -> t.setText(""));
+        lblRFID.setText(SCAN_RFID_MESSAGE);
+    }
+
+    private void getPersonIfValid(Consumer<Person> whenValid, Consumer<String> whenNotValid) {
+        boolean isEmpty
+                = Stream.of(txtAge, txtFirstName, txtJob, txtLastName)
+                .map(t -> t.getText())
+                .map(String::isEmpty)
+                .reduce(Boolean::logicalAnd)
+                .get();
+        boolean isAgeNotANumber = !Pattern.matches("[0-9]+", txtAge.getText());
+        boolean missingRFID = lblRFID.getUserData() == null;
+
+        if (missingRFID) {
+            whenNotValid.accept("Please, scan a RFID");
+        } else if (isEmpty) {
+            whenNotValid.accept("Please fill all fields");
+        } else if (isAgeNotANumber) {
+            whenNotValid.accept("Age must be a number");
+        } else {
+            whenValid.accept(new Person(
+                    txtFirstName.getText(),
+                    txtLastName.getText(),
+                    String.valueOf(lblRFID.getUserData()),
+                    txtJob.getText(),
+                    Integer.parseInt(txtAge.getText())
+            ));
+        }
     }
 }
